c43b940670b749437c3bd288aeead4c1a14d8f67
Chaitanya-Gautam1 <121624935+Chaitanya-Gautam1@users.noreply.github.com>
2025-02-19 15:59:20 +0530

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,8 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
+info.setScore(0)
+continue;
+music.play(music.stringPlayable("D F E G F D G E ", 120), music.PlaybackMode.LoopingInBackground)
+continue;
+game.showLongText("Press \"A\" To start the game!", DialogLayout.Bottom)
---
+++ main.ts
@@ -0,0 +1,6 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
+info.setScore(0)
+music.play(music.stringPlayable("D F E G F D G E ", 120), music.PlaybackMode.LoopingInBackground)
+game.showLongText("Press \"A\" To start the game!", DialogLayout.Bottom)
