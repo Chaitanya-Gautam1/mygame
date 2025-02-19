884f85bb15437460b92571efd94df4885a029978
Chaitanya-Gautam1 <121624935+Chaitanya-Gautam1@users.noreply.github.com>
2025-02-19 18:48:17 +0530

    Update pxt.json, main.blocks, main.ts, main.py, images.g.jres, images.g.ts

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-	
-})
-info.setScore(0)
-continue;
-music.play(music.stringPlayable("D F E G F D G E ", 120), music.PlaybackMode.LoopingInBackground)
-continue;
-game.showLongText("Press \"A\" To start the game!", DialogLayout.Bottom)
+@namespace
+class SpriteKind:
+    background = SpriteKind.create()
+
+def on_a_pressed():
+    global Playing
+    if Playing == 0:
+        music.play(music.string_playable("D F E G F D G E ", 120),
+            music.PlaybackMode.LOOPING_IN_BACKGROUND)
+        Playing = 1
+        scene.camera_shake(2, 2000)
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
+
+Playing = 0
+info.set_score(0)
+Background = sprites.create(assets.image("""
+    cityscape2
+"""), SpriteKind.background)
+game.show_long_text("Press \"A\" To start the game!", DialogLayout.BOTTOM)
--- main.ts
+++ main.ts
@@ -1,6 +1,19 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+namespace SpriteKind {
+    export const background = SpriteKind.create()
+}
+
+controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
+    
+    if (Playing == 0) {
+        music.play(music.stringPlayable("D F E G F D G E ", 120), music.PlaybackMode.LoopingInBackground)
+        Playing = 1
+        scene.cameraShake(2, 2000)
+    }
+    
 })
+let Playing = 0
 info.setScore(0)
-music.play(music.stringPlayable("D F E G F D G E ", 120), music.PlaybackMode.LoopingInBackground)
+let Background = sprites.create(assets.image`
+    cityscape2
+`, SpriteKind.background)
 game.showLongText("Press \"A\" To start the game!", DialogLayout.Bottom)
